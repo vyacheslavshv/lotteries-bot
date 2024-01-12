201fa321309bdb27e090480201de9f5de99fc902
Vyacheslav Shumkov <vyacheslavshumkov@gmail.com>
2024-01-12 18:29:32 +0500

    Fix user model

--- bot/models/user_models.py
+++ bot/models/user_models.py
@@ -6,7 +6,7 @@ class User(Model):
     id = fields.BigIntField(pk=True)
     name = fields.TextField()
     username = fields.TextField(null=True)
-    date = fields.DateField()
+    last_activity = fields.DatetimeField(auto_now=True)
     balance = fields.DecimalField(max_digits=11, decimal_places=2, default=0.0)
     referred_by = fields.ForeignKeyField('models.User', related_name='referrals', null=True)
     is_admin = fields.BooleanField(default=False)

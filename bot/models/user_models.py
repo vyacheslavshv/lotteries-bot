51c838665b513b4be5072b24342b685e4a844cd9
Vyacheslav Shumkov <vyacheslavshumkov@gmail.com>
2024-01-12 18:10:35 +0500

    Add user model

--- bot/models/user_models.py
+++ bot/models/user_models.py
@@ -3,5 +3,14 @@ from tortoise import fields
 
 
 class User(Model):
-    id = fields.IntField(pk=True)
+    id = fields.BigIntField(pk=True)
     name = fields.TextField()
+    username = fields.TextField(null=True)
+    date = fields.DateField()
+    balance = fields.DecimalField(max_digits=11, decimal_places=2, default=0.0)
+    referred_by = fields.ForeignKeyField('models.User', related_name='referrals', null=True)
+    is_admin = fields.BooleanField(default=False)
+    is_banned = fields.BooleanField(default=False)
+
+    # Backward FK relation
+    referrals = fields.ReverseRelation['User']

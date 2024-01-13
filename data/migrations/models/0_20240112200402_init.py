02e084346f23a1f69853100d48e7b559d711b1ae
Vyacheslav Shumkov <vyacheslavshumkov@gmail.com>
2024-01-13 17:29:10 +0500

    Basic setup

---
+++ data/migrations/models/0_20240112200402_init.py
@@ -0,0 +1,26 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "user" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "name" TEXT,
+    "username" TEXT,
+    "last_activity" TIMESTAMP NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "balance" VARCHAR(40) NOT NULL  DEFAULT 0,
+    "is_admin" INT NOT NULL  DEFAULT 0,
+    "is_banned" INT NOT NULL  DEFAULT 0,
+    "referred_by_id" BIGINT REFERENCES "user" ("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSON NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """

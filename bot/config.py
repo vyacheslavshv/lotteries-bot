b270586e134a8dc11ad326a6013327a36fd148c7
Vyacheslav Shumkov <vyacheslavshumkov@gmail.com>
2024-01-14 21:44:43 +0500

    Change string session to file

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
-BOT_SESSION = os.getenv('BOT_SESSION')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 BOT_NAME = os.getenv('BOT_NAME')
 BOT_CHANNEL = os.getenv('BOT_CHANNEL')
 
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import asyncio
+
+from asyncio import CancelledError
 from tortoise import Tortoise
 from telethon import TelegramClient
-from telethon.sessions import StringSession
-from bot.config import API_ID, API_HASH, BOT_SESSION, TORTOISE_ORM
+from bot.config import API_ID, API_HASH, BOT_TOKEN, TORTOISE_ORM
 from bot.handlers.message_handlers import setup_message_handlers
 from bot.handlers.common_handlers import setup_common_handlers
 from bot.handlers.guides.guides_hundlers import setup_guides_handlers
-
 
 
 async def main():
     # Initialize Tortoise ORM
     await Tortoise.init(TORTOISE_ORM)
 
-    async with TelegramClient(StringSession(BOT_SESSION), API_ID, API_HASH) as client:
+    client = TelegramClient('../data/bot', API_ID, API_HASH)
 
-        setup_message_handlers(client)
-        setup_common_handlers(client)
-        setup_guides_handlers(client)
+    await client.start(bot_token=BOT_TOKEN)
 
+    setup_message_handlers(client)
+    setup_common_handlers(client)
+    setup_guides_handlers(client)
+
+    try:
         await client.run_until_disconnected()
+    except CancelledError:
+        pass
+    finally:
+        await client.disconnect()
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        exit()
+    asyncio.run(main())
